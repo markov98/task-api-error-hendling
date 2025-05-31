5504b92d1e43497cbb5fbade019178292855a248
Martin Markov <marto98nm@gmail.com>
2025-05-31 20:22:38 +0300

    adding new methods in itemService

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -5,14 +5,56 @@ const items: Item[] = [];
 
 export const getItems = () => {
     return items;
-}
+};
+
+export const getItem = (id: string) => {
+    if (!id) {
+        throw new Error('Id is required!');
+    }
+
+    const item = items.find(item => item.id === id);
+
+    if (!item) {
+        throw new Error('Item not found!');
+    }
+    return item;
+};
 
 export const addItem = (name: string) => {
     if (!name) {
-        throw new Error('Name is required!')
+        throw new Error('Name is required!');
     }
 
     const newItem: Item = { id: uuidv4(), name };
     items.push(newItem);
     return newItem;
-}
+};
+
+export const editItem = (id: string, name: string) => {
+    if (!name) {
+        throw new Error('Name is required!');
+    }
+
+    if (!id) {
+        throw new Error('Id is required!');
+    }
+
+    const item = items.find(item => item.id === id);
+    if (!item) {
+        throw new Error('Item not found!');
+    }
+
+    item.name = name;
+    return item;
+};
+
+export const deleteItem = (id: string) => {
+    const index = items.findIndex(item => item.id === id);
+    if (index === -1) {
+        throw new Error('Item not found!');
+    }
+
+    items.splice(index, 1)[0];
+    
+    return 'Item successfully deleted!';
+};
